1bb2c4a11b9489ee2838dc820e682f29335c6753
RomaDel1fe <romadelife@gmail.com>
2023-06-11 22:52:47 +0300

    adaptation of the application

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,21 @@ import React from "react";
 import './Header.css'
 
 class Header extends React.Component{
+
   render(){
-    return <header className="Header">Header</header>
+    const buttonClass = this.props.isMenuOpen ? "menu__icon icon-menu menu-open" : "menu__icon icon-menu ";
+    return <header className="Header">
+      <div className="HeaderСontainer">
+      <div class="HeaderActions">
+        <button type="button" 
+          className={buttonClass}
+          onClick={this.props.onToggleMenu}
+        >
+          <span></span>
+        </button>
+      </div>
+      </div>
+    </header>
   }
 }
 
